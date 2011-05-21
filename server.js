69a9ef1bad47ee49b29e6ae8e97c77b0c0246ce3
Artur Adib <arturadib@gmail.com>
2011-05-21 12:50:53 -0400

    loading icon; get.domains returns item count

--- server.js
+++ server.js
@@ -9,17 +9,37 @@ var app = express.createServer();
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/list.domains', function(req, res){
+app.get('/get.domains', function(req, res){
   var sdb = new simpledb.SimpleDB({ keyid:req.query.keyid, secret: req.query.secret});  
-  sdb.listDomains(function(_err, _res, _meta){
-    res.send(JSON.stringify(_res));
+  sdb.listDomains(function(errDomains, resDomains, metaDomains){
+    if (errDomains) throw errDomains;
+    
+    // Gets all counts, in parallel
+    var totalResponses = 0;
+    var domainsObj = [];
+    for (var i=0;i<resDomains.length;i++) {
+      (function(){
+        var j=i;
+        sdb.select("select count(*) from "+resDomains[j], function(errCount, resCount, metaCount){
+          var obj = {};
+          obj.name = resDomains[j];
+          obj.count = resCount[0].Count;
+          domainsObj.push(obj);
+        
+          totalResponses++;
+          if (totalResponses == resDomains.length) { // last one?
+            res.send(domainsObj);
+          }
+        }); // sdb.select
+      })(); // anonymous
+    }    
   });
 });
 
 app.get('/get.items', function(req, res){
   var sdb = new simpledb.SimpleDB({ keyid:req.query.keyid, secret: req.query.secret});  
   sdb.select("select * from "+req.query.domain+" limit 200", function(_err, _res, _meta){
-    res.send(JSON.stringify(_res));
+    res.send(_res);
   });
 });
 
