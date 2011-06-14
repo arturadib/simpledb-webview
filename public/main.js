383a8e67971fb9e09fc2353fa2b257c8aa2cf2e9
Artur Adib <arturadib@gmail.com>
2011-06-14 10:04:18 -0400

    First Backbone commit

--- public/main.js
+++ public/main.js
@@ -1,34 +1,162 @@
 $(function(){       
-  $('button').button();
-  
-  // about
-  $('#about-dialog').dialog({
-    autoOpen: false,
-    width: 400,
-    title: 'About',
-    modal: true,
-    buttons: {
-      Ok: function() {
-        $(this).dialog('close');
+
+  //
+  // Credentials Model
+  //
+  Credentials = Backbone.Model.extend({
+    initialize: function(){
+      _.bindAll(this, 'save', 'validate', 'fetch');
+      this.fetch();
+    },
+    defaults: {
+      keyid: '',
+      secret: ''
+    },
+    validate: function(attr){
+      if (typeof attr === 'undefined') {
+        var attr = this.attributes;        
       }
-    }         
+      if (attr.keyid.length===0 || attr.secret.length===0) {
+        return "Invalid credentials";
+      }
+    },
+    fetch: function(){
+      if ($.cookie('aws-credentials')) {
+        var creds = JSON.parse($.cookie('aws-credentials'));
+        this.set(creds);
+      }
+    },
+    save: function(){
+      var error = this.validate();
+      if (!error) {
+        $.cookie('aws-credentials', JSON.stringify(this.toJSON()), {path:"/", expires:30}); // expires in 1 month
+      }
+    }
   });
+  credentials = new Credentials();
+
   
-  // Layout
-  $('body').layout({ 
-    applyDefaultStyles: true,
-    spacing_open: 4,
-    north__spacing_open: 0,
-    north__size: 50,
-    north__resizable: false,
-    north__closable: false,
-    west__minSize: 250
+  //
+  // Credentials View
+  //
+  CredentialsView = Backbone.View.extend({
+    el: $('.credentials'),
+    events: {
+      "blur input": 'updateModel'
+    },
+    initialize: function(){
+      _.bindAll(this, 'render', 'updateModel');
+      credentials.bind('change', this.render);
+      this.render();
+    },
+    render: function(){
+      this.$('input#keyid').val( credentials.get('keyid') );
+      this.$('input#secret').val( credentials.get('secret') );
+    },
+    updateModel: function(){
+      credentials.set({
+        keyid: $.trim( this.$('input#keyid').val() ),
+        secret: $.trim( this.$('input#secret').val() )
+      }, {
+        silent: true // prevents model2Ui-ui2Model infinite loop
+      });
+    }
   });
-          
+  credentialsView = new CredentialsView();
+
+
+  //
+  // Domains Model
+  //
+  Domains = Backbone.View.extend({    
+  });
+  domains = new Domains();
+
+  
+  //
+  // Domains View
+  //
+  DomainsView = Backbone.View.extend({
+    el: $('#domains'),
+  });
+
+  
+  //
+  // App View
+  //
+  App = Backbone.View.extend({
+    el: $('body'),
+    events: {
+      "click button#view": 'showDomains'
+    },
+    initialize: function(){
+      _.bindAll(this, 'render');
+      this.render();
+    },
+  
+    render: function(){
+      // layout
+      $(this.el).layout({ 
+        applyDefaultStyles: true,
+        spacing_open: 4,
+        north__spacing_open: 0,
+        north__size: 50,
+        north__resizable: false,
+        north__closable: false,
+        west__minSize: 250
+      });
+  
+      // buttons
+      this.$('button').button();
+  
+      // about dialog
+      this.$('#about-dialog').dialog({
+        autoOpen: false,
+        width: 400,
+        title: 'About',
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $(this).dialog('close');
+          }
+        }   
+      }); // about dialog  
+    }, // render
+    
+    
+    showDomains: function(){
+      var credentialsError = credentials.validate();
+      if (credentialsError) {
+        alert('Error: '+credentialsError);
+      }
+      else {
+        credentials.save();
+        // $.cookie('aws-credentials', JSON.stringify(credentials.toJSON()), {path:"/", expires:30}); // expires in 1 month
+        // $('.ui-layout-west .contents').html('');
+        // $('.ui-layout-center .contents').html('');
+        // getDomains();
+      }
+    }
+    
+    // getCredentialsInput: function(){
+    //   return {
+    //     keyid:$.trim($('input#keyid').val()), 
+    //     secret:$.trim($('input#secret').val())
+    //   };
+    // }
+    
+  });
+
+  
+  //
+  // Instances
+  //
+  app = new App();
+
+  
+/*
+  
   // Cookie management
-  function getCredentialsInput(){
-    return {keyid:$.trim($('input#keyid').val()), secret:$.trim($('input#secret').val())};
-  }
   if ($.cookie('aws-credentials')) {
     var creds = JSON.parse($.cookie('aws-credentials'));
     $('input#keyid').val(creds.keyid);
@@ -90,7 +218,7 @@ $(function(){
       $tableBody.height( $parentObj.height() - $tableBody.siblings('.headtable').height() - 2); // see DOM after $.fixheadertable()
     }
   }
-
+  
   // displayData()
   function displayData(data){          
     // build fieldNames from heterogeneous data
@@ -102,7 +230,7 @@ $(function(){
       }
     });
     fieldNames.sort();
-
+  
     // build table head
     var $table = $('<table cellpadding="0" cellspacing="0" border="0" class="display" id="the-table"></table>');
     var $tHead = $('<thead></thead>').appendTo($table);
@@ -111,7 +239,7 @@ $(function(){
       $('<th></th>').html(field).appendTo($tHead);
       templateStr += '<td>${'+encodeHtml(field)+'}</td>';
     });
-
+  
     // build table body
     var $tBody = $('<tbody></tbody>').appendTo($table);
     templateStr = '<tr>'+templateStr+'</tr>';
@@ -121,7 +249,7 @@ $(function(){
     $table.appendTo('.ui-layout-center .contents');
     displayTable($table);
   }
-
+  
   // getItems()
   function getItems($obj){
     $('.ui-layout-center .contents').html('');
@@ -134,7 +262,7 @@ $(function(){
       displayData(data);
     }, 'json');
   }
-
+  
   // click: view domains button
   $('button#view').click(function(){          
     if (getCredentialsInput().keyid.length>0 && getCredentialsInput().secret.length>0) {
@@ -160,11 +288,13 @@ $(function(){
   $('button#about').click(function(){
     $('#about-dialog').dialog('open');
   });
-
+  
   // click: domain contents
   $('table tr').live('click', function(e){
     $(this).siblings('.selected').removeClass('selected');
     $(this).addClass('selected');
   });
+
+*/
 
 });
