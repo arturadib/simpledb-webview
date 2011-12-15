ad8ba4cfbab60c77dfa2ef40e9a1a95974a0fd61
Bjoern Boettcher <doitdistributed@parallel-ing.net>
2011-12-15 13:07:48 +0100

    removed no longer supported require.paths

--- server.js
+++ server.js
@@ -2,9 +2,6 @@
 // server.js for SimpleDB WebView
 //
 
-// We keep our own modules separate from npm modules (which go into node_modules)
-require.paths.unshift(__dirname + "/custom_modules/");
-
 var express = require('express');
 var simpledb = require('simpledb');
 
